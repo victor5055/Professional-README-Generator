331c1a20c622686a6a8cccad91812e02a6fccbfe
Victor <victor5055@outlook.com>
2022-10-31 16:20:29 -0400

    Adding my questions in index.js.

---
+++ index.js
@@ -0,0 +1,58 @@
+var fileGenerator = require("./fileGenerator");
+var fs = require("fs");
+var inquirer = require('inquirer');
+
+// Questions to ask the user //
+let questions = [
+    {
+        type: "input",
+        mesage: "What is the title of your project?",
+        name: "title",
+    },{
+        type:"input",
+        message:"What is your Github user name?",
+        name: "userName"
+    },{
+        type:"input",
+        message:"Give your Github profile link?",
+        name: "GitHub"
+    },{
+        type:"input",
+        message:"Provide a breif summary of the project?",
+        name: "summary"
+    },{
+        type:"input",
+        message:"Give your logo information?",
+        name: "logo"
+    },{
+        type:"input",
+        message:"How do you install this project?",
+        name: "installation"
+    },{
+        type:"input",
+        message:"What is the Deployment link?",
+        name: "deploymentLink"
+    },{
+        type:"input",
+        message:"What function does this project perform?",
+        name: "functionality"
+    },{
+        type:"input",
+        message:"List your collaborators and any attributions that need to be made?",
+        name: "credits"
+    },{
+        type:"input",
+        message:"How do you want to license this project?",
+        name: "license",
+        choices: [
+            "Apache",
+            "Academic",
+            "GNU",
+            "ISC",
+            "MIT",
+            "Mozilla",
+            "Open"
+        ]
+    },{
+    
+];
