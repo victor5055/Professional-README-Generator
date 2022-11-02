b2b28c45b196025b49f57853892097cbc28c3bd7
Victor <victor5055@outlook.com>
2022-11-02 16:39:05 -0400

    Table of contents discription of repo.

--- fileGenerator.js
+++ fileGenerator.js
@@ -2,3 +2,12 @@ function fileGenerator(response) {
     var content = `# ${response.title}
     #### ${response.logo}[${response.userName}](${response.GitHub}) [Email Me](${response.email}) [Deployed Application]
     #### READMEGenerator© 2022 All Rights Reserved.  ${response.license} (free to use).
+
+    ## Table of Contents
+    * [Description of Repository](#Repository-Description)
+   
+    
+    # Description of Repository
+    ###### [Back to Table of Contents](#Table-of-Contents)
+    ${response.description}
+    
